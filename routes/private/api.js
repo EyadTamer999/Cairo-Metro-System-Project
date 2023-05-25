5dc4e9f6b95e1ebd17fd45f060c047259c3bceb6
Mahmouddd21 <mahmoudosama11.msed@gmail.com>
2023-05-25 21:04:25 +0300

    Delete api route done

--- routes/private/api.js
+++ routes/private/api.js
@@ -175,11 +175,19 @@ module.exports = function (app) {
   });
 
   // -Delete route: NOT DONE {Testing}
-  app.delete("/api/v1/route/:routeId", async (req, res) => {
+  app.delete("/api/v1/route/:routeid", async (req, res) => {
 
     // need to check whethter the user is an admin or not
     const user = await getUser(req);
     if (!user.isAdmin) return res.status(401);
+
+    const { routeid } = req.params;
+    console.log(routeid);
+    console.log(typeof routeid);
+
+    const routeId = parseInt(routeid);
+    console.log(routeId);
+    console.log(typeof routeId);
 
     // try to see if the route already exists
     const existRoute = await db("se_project.routes")
@@ -190,43 +198,63 @@ module.exports = function (app) {
       return res.status(404).send("Route does not exist");
     }
 
-    const { routeId } = req.params;
 
     const fromST = await db("se_project.routes")
       .where({ id: routeId })
-      .select("fromStationid")
+      .select("fromstationid")
       .first();
+      console.log(fromST);
+      console.log(typeof fromST);
+      const fromSTid = fromST.fromstationid;
+      console.log(fromSTid);
+      console.log(typeof fromSTid);
     const toST = await db("se_project.routes")
       .where({ id: routeId })
-      .select("toStationid")
+      .select("tostationid")
       .first();
+      console.log(toST);
+      console.log(typeof toST);
+      const toSTid = toST.tostationid;
+      console.log(toSTid);
+      console.log(typeof toSTid);
 
     const fstation = await db("se_project.stations")
-      .where({ id: fromST })
+      .where({ id: fromSTid })
       .select("stationposition")
       .first();
+      console.log(fstation);
+      console.log(typeof fstation);
+      const fstationpos = fstation.stationposition;
+      console.log(fstationpos);
+      console.log(typeof fstationpos);
+
     const tstation = await db("se_project.stations")
-      .where({ id: toST })
+      .where({ id: toSTid })
       .select("stationposition")
       .first();
+      console.log(tstation);
+      console.log(typeof tstation);
+      const tstationpos = tstation.stationposition;
+      console.log(tstationpos);
+      console.log(typeof tstationpos);
 
     //case for the start
-    if (fstation === "start") {
+    if (fstationpos === "start") {
       const othRoute = await db("se_project.routes")
-        .where({ id: routeId, toStationid: fromST })
+        .where({ id: routeId, tostationid: fromSTid })
         .select("*")
         .first();
 
       if (!othRoute) {
         try {
           await db("se_project.stations")
-            .where({ id: fromST })
+            .where({ id: fromSTid })
             .update({ stationstatus: "inactive" })
             .returning("*");
           //change the position of the connected station
           await db("se_project.stations")
-            .where({ id: toST })
-            .update({ stationposition: fstation })
+            .where({ id: toSTid })
+            .update({ stationposition: fstationpos })
             .returning("*");
         } catch (error) {
           return res.status(500).send(error);
@@ -234,21 +262,21 @@ module.exports = function (app) {
       }
     }
     //case for the end
-    if (tstation === "end") {
+    if (tstationpos === "end") {
       const othRoute = db("se_project.routes")
-        .where({ id: routeId, fromStationid: toST })
+        .where({ id: routeId, fromstationid: toSTid })
         .select("*")
         .first();
 
       if (!othRoute) {
         db("se_project.stations")
-          .where({ id: toST })
+          .where({ id: toSTid })
           .update({ stationstatus: "inactive" })
           .returning("*");
         //change the position of the connected station
         db("se_project.stations")
-          .where({ id: fromST })
-          .update({ stationposition: tstation })
+          .where({ id: fromSTid })
+          .update({ stationposition: tstationpos })
           .returning("*");
       }
     }
