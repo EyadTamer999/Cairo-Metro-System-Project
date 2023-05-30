5539a581afce17db77989394d65ca573db477f26
EyadTamer999 <eyadhassan2002@gmail.com>
2023-05-30 19:12:00 +0300

    Done with login page

--- routes/public/view.js
+++ routes/public/view.js
@@ -8,17 +8,16 @@ module.exports = function(app) {
 
   // Register HTTP endpoint to render /login page
   app.get('/login', function(req, res) {
-    return res.render('login.html', {title: "Cairo Metro"});
+    return res.render('login.html', {title: "Login"});
   });
 // example of passing variables with a page
   app.get('/register', async function(req, res) {
-    const stations = await db.select('*').from('se_project.stations');
-    return res.render('register.html', { stations });
+    return res.render('register.html');
   });
 
   // example of passing variables with a page
   app.get('/dashboard', async function(req, res) {
     //Todo send user info via session to display things like name subscription etc. THIS SHOULD BE IN PRIVATE
-    return res.render('dashboard.html',{firstName:"still not set"});
+    return res.render('dashboard.html');
   });
 };
